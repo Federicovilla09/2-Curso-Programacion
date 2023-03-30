fba8f8848335c3d89b54234d8ac3c4c9b2341523
Federico Villanueva <federubents@gmail.com>
2023-03-29 23:37:39 -0300

    Declaración lenta de variables

--- mokepon/script.js
+++ mokepon/script.js
@@ -9,9 +9,7 @@ const botonReiniciar = document.getElementById("boton-reiniciar");
 const sectionSeleccionarMascota = document.getElementById(
   "seleccionar-mascota"
 );
-const inputHipodoge = document.getElementById("hipodoge");
-const inputCapipepo = document.getElementById("capipepo");
-const inputRatigueya = document.getElementById("ratigueya");
+
 const spanMascotaJugador = document.getElementById("mascota-jugador");
 
 const spanMascotaEnemigo = document.getElementById("mascota-enemigo");
@@ -28,6 +26,9 @@ let mokepones = [];
 let ataqueJugador;
 let ataqueEnemigo;
 let opcionDeMokepones;
+let inputHipodoge;
+let inputCapipepo;
+let inputRatigueya;
 let vidasJugador = 3;
 let vidasEnemigo = 3;
 
@@ -97,12 +98,16 @@ function iniciarJuego() {
           />
         </label>`;
 
-    contenedorTarjetas.innerHTML += opcionDeMokepones;   
+    contenedorTarjetas.innerHTML += opcionDeMokepones;
+
+    inputHipodoge = document.getElementById("Hipodoge")
+    inputCapipepo = document.getElementById("Capipepo")
+    inputRatigueya = document.getElementById("Ratigueya")
   });
 
   sectionReiniciar.style.display = "none";
   botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
-  
+
   botonFuego.addEventListener("click", ataqueFuego);
   botonAgua.addEventListener("click", ataqueAgua);
   botonTierra.addEventListener("click", ataqueTierra);
